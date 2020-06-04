0a348bab13d22965682146317cfbfa3385fcc747
anordy <anordypatrick98@gmail.com>
2020-06-04 13:32:42 +0300

    initializing views

--- musics/urls.py
+++ musics/urls.py
@@ -5,9 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.musics, name='musics'),
     path('dashboards/', views.dashboards, name='dashboards'),
+    path('songs/', views.songs, name= 'songs'),
+    path('albums/', views.albums, name= 'albums'),
+    path('artists/', views.artists, name= 'artists'),
 
-    # path('songs/', views.songs, name= 'songs'),
-    # path('albums/', views.albums, name= 'albums'),
     # path('organizations/', views.organizations, name= 'organizations')
 
 ]
